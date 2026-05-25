d1ac9797a05da56e6a498c373691bc9b4a693bb7
brahmmareddy-123 <bandibrahmmareddy@gmail.com>
2026-05-25 10:19:38 +0530

    Connected Frontend to deployed backend

--- frontend/script.js
+++ frontend/script.js
@@ -35,7 +35,7 @@ async function predictRisk() {
             education > 14 ? 8 : 3
         ];
 
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://financial-risk-analysis-system.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
